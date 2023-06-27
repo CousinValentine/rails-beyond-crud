3e9b977a20895409e1b627db5d4c271b3b51afb9
valentinecousin <valentine.cousin@gmail.com>
2023-06-27 21:49:07 +0200

    reviews table created

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,17 @@
 Rails.application.routes.draw do
+  get 'reviews/new'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
   # root to: 'restaurants#index'
   resources :restaurants do
-    collection do
-      get :top
-    end
-    member do
-      get :chef
-    end
+    resources :reviews, only: [:new]
+  #   collection do
+  #     get :top
+  #   end
+  #   member do
+  #     get :chef
+  #   end
   end
 end
